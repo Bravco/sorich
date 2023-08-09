3c735d87207409f2785e57f8519e0c0fd84d40fe
Bravco <boss.wiw@gmail.com>
2023-08-09 08:04:07 +0200

    Retrieving product by id from database

--- sorich/pages/products/[id].vue
+++ sorich/pages/products/[id].vue
@@ -1,9 +1,12 @@
 <template>
     <div>
-        
+
     </div>
 </template>
 
 <script setup>
     const { id: productId } = useRoute().params;
+    const medusaClient = useMedusaClient();
+
+    const { product } = await medusaClient.products.retrieve(productId);
 </script>
