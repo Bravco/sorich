0ead2773520c8e65c098adc658ee9e18742fb22d
Bravco <boss.wiw@gmail.com>
2023-08-16 11:46:00 +0200

    Added checkout stages

--- sorich/pages/checkout.vue
+++ sorich/pages/checkout.vue
@@ -6,64 +6,74 @@
                 <ul class="process-list">
                     <li class="process-item">
                         <h2 class="process-heading">Kontakt</h2>
-                        <form @submit.prevent="" class="process-content">
+                        <span v-if="Stage.CONTACT < stage" class="completed-icon-container">
+                            <Icon name="mdi:check" size="1.5rem"/>
+                        </span>
+                        <form v-if="stage === Stage.CONTACT" @submit.prevent="stage = Stage.ADDRESS" class="process-content">
                             <div class="inputfield">
                                 <label for="email">Email</label>
-                                <input type="email" name="email" id="email" required>
+                                <input v-model="data.email" type="email" name="email" id="email" placeholder="example@example.com" required>
                             </div>
                             <div class="inputfield">
                                 <label for="phone">Telefón</label>
-                                <input type="tel" name="phone" id="phone">
+                                <input v-model="data.phone" type="tel" name="phone" id="phone" placeholder="+421 123 123 123">
                             </div>
                             <div class="process-footer">
                                 <button class="next-btn" type="submit" aria-label="Ďalej">Ďalej</button>
                             </div>
                         </form>
-                        
                     </li>
                     <li class="process-item">
                         <h2 class="process-heading">Adresa</h2>
-                        <form @submit.prevent="" class="process-content">
+                        <span v-if="Stage.ADDRESS < stage" class="completed-icon-container">
+                            <Icon name="mdi:check" size="1.5rem"/>
+                        </span>
+                        <form v-if="stage === Stage.ADDRESS" @submit.prevent="stage = Stage.DELIVERY" class="process-content">
                             <div class="inputfield-row">
                                 <div class="inputfield">
                                     <label for="first-name">Krstné meno</label>
-                                    <input type="text" name="first-name" id="first-name" required>
+                                    <input v-model="data.first_name" type="text" name="first-name" id="first-name" placeholder="Stefan" required>
                                 </div>
                                 <div class="inputfield">
                                     <label for="last-name">Priezvisko</label>
-                                    <input type="text" name="last-name" id="last-name" required>
+                                    <input v-model="data.last_name" type="text" name="last-name" id="last-name" placeholder="Baca" required>
                                 </div>
                             </div>
                             <div class="inputfield-row">
                                 <div class="inputfield">
-                                    <label for="country">Krajina</label>
-                                    <input type="text" name="country" id="country" required>
+                                    <label for="country-code">Krajina</label>
+                                    <select v-model="data.country_code" name="country-code" id="country-code" required>
+                                        <option value="sk">Slovensko</option>
+                                        <option value="cz">Česká Republika</option>
+                                    </select>
                                 </div>
                                 <div class="inputfield">
                                     <label for="city">Obec</label>
-                                    <input type="text" name="city" id="city" required>
+                                    <input v-model="data.city" type="text" name="city" id="city" placeholder="Kosice" required>
                                 </div>
                             </div>
                             <div class="inputfield-row">
                                 <div class="inputfield">
-                                    <label for="province">Provincia</label>
-                                    <input type="text" name="province" id="province">
+                                    <label for="province">Oblasť</label>
+                                    <input v-model="data.province" type="text" name="province" id="province" placeholder="Kosicky kraj">
                                 </div>
                                 <div class="inputfield">
                                     <label for="postal-code">PSČ</label>
-                                    <input type="text" name="postal-code" id="postal-code" required>
+                                    <input v-model="data.postal_code" type="text" name="postal-code" id="postal-code" placeholder="040 01" required>
                                 </div>
                             </div>
-                            <div class="inputfield">
-                                <label for="address1">Adresa</label>
-                                <input type="text" name="address1" id="address1" required>
-                            </div>
-                            <div class="inputfield">
-                                <label for="address2">Byt, Apartmán a pod.</label>
-                                <input type="text" name="address2" id="address2">
+                            <div class="inputfield-row">
+                                <div class="inputfield">
+                                    <label for="address1">Adresa 1</label>
+                                    <input v-model="data.address_1" type="text" name="address1" id="address1" placeholder="Farmarska 13" required>
+                                </div>
+                                <div class="inputfield">
+                                    <label for="address2">Adresa 2</label>
+                                    <input v-model="data.address_2" type="text" name="address2" id="address2" placeholder="Stefanikova 5">
+                                </div>
                             </div>
                             <div class="process-footer">
-                                <button class="back-btn" aria-label="Späť">
+                                <button @click.prevent="stage = Stage.CONTACT" class="back-btn" aria-label="Späť">
                                     <Icon name="uil:angle-left-b"/>
                                     Späť
                                 </button>
@@ -73,9 +83,12 @@
                     </li>
                     <li class="process-item">
                         <h2 class="process-heading">Donáška</h2>
-                        <form submit.prevent="" class="process-content">
+                        <span v-if="Stage.DELIVERY < stage" class="completed-icon-container">
+                            <Icon name="mdi:check" size="1.5rem"/>
+                        </span>
+                        <form v-if="stage === Stage.DELIVERY" @submit.prevent="stage = Stage.PAYMENT" class="process-content">
                             <div class="process-footer">
-                                <button class="back-btn" aria-label="Späť">
+                                <button @click.prevent="stage = Stage.ADDRESS" class="back-btn" aria-label="Späť">
                                     <Icon name="uil:angle-left-b"/>
                                     Späť
                                 </button>
@@ -85,9 +98,9 @@
                     </li>
                     <li class="process-item">
                         <h2 class="process-heading">Platba</h2>
-                        <form submit.prevent="" class="process-content">
+                        <form v-if="stage === Stage.PAYMENT" @submit.prevent="" class="process-content">
                             <div class="process-footer">
-                                <button class="back-btn" aria-label="Späť">
+                                <button @click.prevent="stage = Stage.DELIVERY" class="back-btn" aria-label="Späť">
                                     <Icon name="uil:angle-left-b"/>
                                     Späť
                                 </button>
@@ -136,6 +149,13 @@
 </template>
 
 <script lang="ts" setup>
+    enum Stage {
+        CONTACT,
+        ADDRESS,
+        DELIVERY,
+        PAYMENT,
+    }
+
     useHead({
         title: "SoRich | Pokladňa",
     });
@@ -143,6 +163,9 @@
     const medusaClient = useMedusaClient();
     const { formatPrice } = useUtils();
     const { cart, setCart } = useCart();
+
+    const stage = ref<Stage>(Stage.CONTACT);
+    const data = ref<any>({});
 </script>
 
 <style scoped>
@@ -175,6 +198,12 @@
         background-color: rgba(0, 0, 0, .25);
     }
 
+    .process-item:has(.completed-icon-container) {
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+    }
+
     .process-content {
         display: flex;
         flex-direction: column;
@@ -185,31 +214,40 @@
         display: flex;
         justify-content: space-between;
         align-items: center;
+        gap: 1rem;
     }
 
     .inputfield {
+        width: 100%;
         display: flex;
         flex-direction: column;
         align-items: flex-start;
         gap: .5rem;
     }
 
-    .inputfield input {
+    .inputfield input, .inputfield select {
         width: 100%;
         padding: .25rem .5rem;
+        color: white;
         border: 1px solid white;
         border-radius: .25rem;
         opacity: .5;
         transition: opacity 300ms;
     }
 
-    .inputfield input:focus {
+    .inputfield select option {
+        color: black;
+    }
+
+    .inputfield input:focus, .inputfield select:focus {
         opacity: 1;
     }
 
-    .inputfield:has(input[required]) label::after {
-        content: " *";
-        color: red;
+    .inputfield:not(:has(input[required]),:has(select[required])) label::after {
+        content: "(Voliteľné)";
+        margin-left: .5rem;
+        font-size: .75rem;
+        opacity: .5;
     }
 
     .process-footer {
@@ -232,6 +270,15 @@
     }
 
     .pay-btn {
+        background-color: var(--color-success);
+    }
+
+    .completed-icon-container {
+        width: 2rem;
+        height: 2rem;
+        display: grid;
+        place-items: center;
+        border-radius: 50%;
         background-color: var(--color-success);
     }
 
