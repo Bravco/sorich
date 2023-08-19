208ac18f29cbd567132d078173f395f2f2d1b06c
Bravco <boss.wiw@gmail.com>
2023-08-19 13:38:01 +0200

    Added meta tags

--- sorich/nuxt.config.ts
+++ sorich/nuxt.config.ts
@@ -18,6 +18,69 @@ export default defineNuxtConfig({
       name: "page",
       mode: "out-in",
     },
+    head: {
+      htmlAttrs: {
+        lang: "sk",
+      },
+      meta: [
+        {
+          name: "robots",
+          content: "robots: index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1",
+        },
+        {
+          name: "author",
+          content: "SoRich",
+        },
+        {
+          name: "title",
+          content: "SoRich SHOP",
+        },
+        {
+          name: "description",
+          content: "SoRich módny eshop, streetwear s roznym štýlovým oblečením a doplnkami.",
+        },
+        {
+          property: "og:type",
+          content: "website",
+        },
+        {
+          property: "og:site_name",
+          content: "SoRich s.r.o.",
+        },
+        {
+          property: "og:url",
+          content: "https://www.sorich.sk",
+        },
+        {
+          property: "og:title",
+          content: "SoRich SHOP",
+        },
+        {
+          property: "og:description",
+          content: "SoRich módny eshop, streetwear s roznym štýlovým oblečením a doplnkami.",
+        },
+        {
+          property: "og:image",
+          content: "/images/home.webp",
+        },
+        {
+          property: "twitter:card",
+          content: "summary_large_image",
+        },
+        {
+          property: "twitter:title",
+          content: "SoRich SHOP",
+        },
+        {
+          property: "twitter:description",
+          content: "SoRich módny eshop, streetwear s roznym štýlovým oblečením a doplnkami.",
+        },
+        {
+          property: "twitter:image",
+          content: "/images/home.webp",
+        },
+      ],
+    },
   },
   runtimeConfig: {
     public: {
